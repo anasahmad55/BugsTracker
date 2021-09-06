9155871125681850f725ccb16fbd74ca8c25a500
Anas Ahmad <ahmadanas4490@gmail.com>
2021-09-06 16:42:55 +0500

    restricted qa from editing and deleting the project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://bugs-tracker1.herokuapp.com/'
+  # host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host }
 
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtppro.zoho.com",
+    :port                 => 465,
+    :user_name            => "anas.ahmad",
+    :password             => "Anas.555",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
